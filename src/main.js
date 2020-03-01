b875a9ac8b65b7e646e3476e99ce9d390288f782
iq7ii <385897888@qq.com>
2020-03-01 16:26:28 +0800

    资源准备

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入element-ui模块
+import ElementUI from 'element-ui'
+// 引入css样式
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.less'
 Vue.config.productionTip = false
+
+// 注册全局组件
+Vue.use(ElementUI)
 
 new Vue({
   router,
