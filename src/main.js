7b3219554c23d04a4725287fa7706638cf376745
iq7ii <385897888@qq.com>
2020-03-02 20:22:06 +0800

    配置axios

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import ElementUI from 'element-ui'
 // 引入css样式
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/styles/index.less'
+import axios from 'axios'
 Vue.config.productionTip = false
+
+// 配置公共的请求头地址
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 将axios赋值给 Vue对象的原型属性
+Vue.prototype.$axios = axios
 
 // 注册全局组件
 Vue.use(ElementUI)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,56 +1,112 @@
 <template>
   <div class="login">
-     <!-- 表单 -->
-     <el-card class="login_card">
-         <!-- logo内容 -->
-         <div class="title">
-            <img src="../../assets/img/logo_index.png" alt="">
-         </div>
-         <!-- 表单 -->
-         <el-form style="margin-top:20px">
-             <el-form-item>
-                 <!-- 表单域 -->
-                  <el-input placeholder="请输入手机号"></el-input>
-             </el-form-item>
-            <el-form-item>
-                   <!-- 验证码 -->
-                <el-input style="width:65%" placeholder="请输入验证码"></el-input>
-                <el-button style="float:right;width:30%" plain>发送验证码</el-button>
-             </el-form-item>
-             <el-form-item>
-            <el-checkbox style="margin-left:20px">我已阅读并同意用户协议和隐私条款</el-checkbox>
-             </el-form-item>
-             <el-form-item>
-                 <el-button type="primary" style="width:100%">登录</el-button>
-             </el-form-item>
-         </el-form>
-     </el-card>
+    <!-- 表单 -->
+    <el-card class="login_card">
+      <!-- logo内容 -->
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <!-- 表单 绑定model属性 ,绑定表单验证规则-->
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:20px">
+        <!-- 设置prop属性 prop表示要校验的字段名 -->
+        <el-form-item prop="mobile">
+          <!-- 表单域 v-model实现双向绑定-->
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <!-- 验证码 -->
+          <el-input v-model="loginForm.code" style="width:65%" placeholder="请输入验证码"></el-input>
+          <el-button style="float:right;width:30%" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="loginForm.checked" style="margin-left:20px;color:#ccc">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login" type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 定义表单数据对象
+      loginForm: {
+        mobile: '',
+        code: '',
+        checked: 'false'
+      },
+      // 定义表单验证规则
+      loginRules: {
+        // required 为true 则表示为必填 不能校验true/false
+        mobile: [{ required: true, message: '手机号不能为空' }, {
+          // 用正则来校验格式是否正确
+          pattern: /^1[3-9]\d{9}$/, // 正则表达式
+          message: '手机号格式错误'
+        }],
+        code: [{ required: true, message: '验证码不能为空' }, {
+          pattern: /^\d{6}$/, // 6个数字
+          message: '验证码格式错误'
+        }],
+        // 自定义校验  规则,值,要校验的函数
+        checked: [{
+          validator: function (rule, value, callback) {
+            //  校验成功执行callback,表示执行通过失败,也执行,但是要抛出一个错误 new Error
+            value ? callback() : callback(new Error('请勾选同意条款'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // validate 两个参数 isOK,fields(没有校验通过的字段)
+      // this.$refs.loginForm.validate(function (isOK) {
+      //   if (isOK) {
+      //     console.log('校验通过')
+      //   } else {
+      //     console.log('未通过')
+      //   }
+      // })
+      this.$refs.loginForm.validate().then(() => {
+        alert('123')
+      })
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
- .login{
+.login {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // 毛玻璃效果
+  &:before {
+    filter: blur(1px);
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
     background-image: url("../../assets/img/login_bg.jpg");
-    height: 100vh;
-    background-size: cover;
-    display: flex;
-    justify-content: center;
-    align-items: center
- }
- .login_card{
-     width: 460px;
-     height: 340px;
-     .title{
-           text-align: center;
-         img{
-             height: 30px;
-          }
-     }
- }
+   background-size: cover;
+  }
+  .login_card {
+    background: rgba(0,0,0,.1);
+    z-index: 2;
+    border-color: transparent;
+    width: 460px;
+    height: 340px;
+    .title {
+      text-align: center;
+      img {
+        height: 30px;
+      }
+    }
+  }
+}
 </style>
