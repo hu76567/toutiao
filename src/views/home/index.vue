c8238852b383f558ab2cca25244fd5f5c7646dd6
iq7ii <385897888@qq.com>
2020-03-02 22:31:02 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-      <h1>主页</h1>
-  </div>
+ <el-container>
+  <el-aside width="200px">
+    <!-- 侧边栏 -->
+    <h1>Aside</h1>
+    </el-aside>
+  <el-container>
+    <!-- 头部 -->
+    <el-header>
+     <h1>Header</h1>
+     </el-header>
+     <!-- 主体 -->
+    <el-main>
+     <h1>Main</h1>
+     <!-- 此处二级容器 -->
+     <router-view></router-view>
+     </el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -11,5 +26,4 @@ export default {
 </script>
 
 <style>
-
 </style>
