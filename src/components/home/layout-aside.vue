215747f9570b8a59d9a4b5e8a5acf3eedd3562f5
iq7ii <385897888@qq.com>
2020-03-03 22:10:33 +0800

    配置左侧导航路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,9 +3,9 @@
       <div class="title">
           <img src="../../assets/img/logo_admin.png" alt="">
       </div>
-      <!-- 导航菜单 -->
-      <el-menu background-color="#323745" text-color="#adafb5">
-         <el-menu-item>
+      <!-- 导航菜单 开启路由模式简写 router -->
+      <el-menu :router="true" background-color="#323745" text-color="#adafb5">
+         <el-menu-item index="/home">
              <i class="el-icon-s-home"></i>
              <span>首页</span>
          </el-menu-item>
@@ -16,10 +16,10 @@
                <span>内容管理</span>
            </template>
              <!-- 二级菜单项 -->
-             <el-menu-item index="2-1">发布文章</el-menu-item>
-             <el-menu-item index="2-2">内容列表</el-menu-item>
-             <el-menu-item index="2-3">屏幕列表</el-menu-item>
-             <el-menu-item index="2-4">素材管理</el-menu-item>
+             <el-menu-item index="/home/publish">发布文章</el-menu-item>
+             <el-menu-item index="/home/articles">内容列表</el-menu-item>
+             <el-menu-item index="/home/comment">屏幕列表</el-menu-item>
+             <el-menu-item index="/home/material">素材管理</el-menu-item>
          </el-submenu>
 
           <el-submenu index=3>
@@ -28,12 +28,12 @@
                <span>粉丝管理</span>
             </template>
              <!-- 二级菜单项 -->
-             <el-menu-item index="3-1">图文数据</el-menu-item>
-             <el-menu-item index="3-2">粉丝概况</el-menu-item>
-             <el-menu-item index="3-3">粉丝画像</el-menu-item>
-             <el-menu-item index="3-4">粉丝列表</el-menu-item>
+             <el-menu-item index="/home/picture">图文数据</el-menu-item>
+             <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+             <el-menu-item index="/home/fansphoto">粉丝画像</el-menu-item>
+             <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
            </el-submenu>
-         <el-menu-item>
+         <el-menu-item index="/home/account">
              <i class="el-icon-user-solid"></i>
               <span>账户信息</span>
          </el-menu-item>
