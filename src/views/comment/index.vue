244522878cb16d132838fd315bf0679f1f6dbd4d
iq7ii <385897888@qq.com>
2020-03-05 16:05:37 +0800

    评论列表结构

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>评论页</div>
+  <el-card>
+      <bread-crumb slot="header">
+        <template slot="title">评论管理</template>
+      </bread-crumb>
+      <el-table>
+        <!-- 表格组件 -->
+        <!-- prop表示现实的字段 -->
+        <!-- el-table-clumn  列 -->
+        <el-table-column width="600" label="标题"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="总评论数"></el-table-column>
+        <el-table-column label="粉丝评论数"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: [{}, {}, {}]
+    }
+  }
 
 }
 </script>
--- src/views/home/sechome.vue
+++ src/views/home/sechome.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="sechome">
-    <el-carousel :interval="4000" type="card"  height="360px">
-    <el-carousel-item v-for="item in list" :key="item">
-      <img class="lbp"  :src="item" alt="">
-    </el-carousel-item>
+     <el-carousel  indicator-position="none" arrow="never" class="bg" height="767.33px">
+        <el-carousel-item v-for="item in list" :key="item">
+          <img class="bgi"  :src="item" alt="">
+        </el-carousel-item>
+         <el-carousel type="card"  height="400px">
+       <el-carousel-item v-for="item in list" :key="item">
+          <img class="zmd"  :src="item" alt="">
+      </el-carousel-item>
   </el-carousel>
+  </el-carousel>
+
   </div>
 </template>
 
@@ -12,8 +18,8 @@
 export default {
   data () {
     return {
-      list: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583339023654&di=7eff28d4054174bf5c6757da67c9990d&imgtype=0&src=http%3A%2F%2Fworld.haiwainet.cn%2FNMediaFile%2F2014%2F1208%2FLOCAL201412081106000494116029555.jpg',
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583256267171&di=07cd103dc44f94c420a552d038f15c53&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F4%2F5147d1f211c31.jpg',
+      list: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583386684746&di=77f52a0c8e63ff0248351a69862dd5a5&imgtype=0&src=http%3A%2F%2Fthumb21.jfcdns.com%2Fn331lx32a1t9166e741%2F16f5cef3cb481c06.jpeg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583386726110&di=405b922a4026ba059ce2846b6d9abd10&imgtype=0&src=http%3A%2F%2Fpicture.ik123.com%2Fuploads%2Fallimg%2F171220%2F4-1G220145F9.jpg',
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583256392780&di=7c6d6f87ef954a0c51bf862e4bcbd2db&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D3037054080%2C4019486712%26fm%3D214%26gp%3D0.jpg',
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583256481750&di=69d5dc213c3624ad94e097fd64e8bb0c&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2F8c1001e93901213f0c02bdca57e736d12f2e95f7.jpg',
         'http://h.hiphotos.baidu.com/zhidao/pic/item/0b55b319ebc4b74524841706cefc1e178a821589.jpg',
@@ -24,7 +30,12 @@ export default {
 </script>
 
 <style>
-   .lbp{
-      height: 360px;
+   .zmd{
+      /* opacity:0.7; */
+      height: 400px;
+   }
+   .bgi{
+     filter: blur(2px);
+     height: 100%;
    }
 </style>
