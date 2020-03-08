f1b753cce4f3043c8cbc9cb8db71d64eb51bcd28
iq7ii <873492647@qq.com>
2020-03-08 11:50:20 +0800

    发布页面简单构造

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -5,6 +5,35 @@
             发布文章
         </template>
      </bread-crumb>
+
+     <!-- 表单组件 -->
+     <el-form style="margin-left:50px" label-width="100px">
+         <el-form-item label="标题">
+            <el-input placeholder="请输入标题" style="width:50%"></el-input>
+         </el-form-item>
+         <el-form-item label="内容">
+             <el-input placeholder="请输入内容" type="textarea" :rows='4'>
+
+             </el-input>
+         </el-form-item>
+         <el-form-item label="封面">
+             <el-radio-group>
+                 <el-radio>单图</el-radio>
+                 <el-radio>三图</el-radio>
+                 <el-radio>无图</el-radio>
+                 <el-radio>自动</el-radio>
+             </el-radio-group>
+         </el-form-item>
+         <el-form-item label="频道">
+             <el-select placeholder="请选择频道">
+
+             </el-select>
+         </el-form-item>
+         <el-form-item>
+             <el-button type="primary">发布</el-button>
+             <el-button>存入草稿</el-button>
+         </el-form-item>
+     </el-form>
  </el-card>
 </template>
 
